05b7c9369ce778426aee805900917867f26bfb9c
Rafliramdhani16 <rafliramdhani773@gmail.com>
2023-09-12 13:22:57 +0700

    Belajar dasar JS

---
+++ script/Hello_World.js
@@ -0,0 +1,5 @@
+document.writeln("Hello World dfusd");
+// ini buat satu baris
+document.writeln("<p>ini komentar 1 baris</p>");
+/*ini untuk banyak baris skbasfbasbfosabsanoasfk*/
+document.writeln("<p>komentar lebih dari satu baris</p>");
